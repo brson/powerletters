17f433f6059bc85f6b1a1532b7755c33cbdb052e
Brian Anderson <andersrb@gmail.com>
2025-10-07 13:05:30 -0600

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,6 @@
 //! Concise spellings of common Rust operations.
 
-/// To `String`.
-#[allow(non_snake_case)]
-pub fn S<T>(s: &T) -> String
-where
-    T: ToString + ?Sized,
-{
-    ToString::to_string(s)
-}
-
-/// Clone.
+/// Power [`Clone`].
 #[allow(non_snake_case)]
 pub fn C<T>(o: &T) -> T
 where
@@ -18,7 +9,7 @@ where
     o.clone()
 }
 
-/// To owned.
+/// Power [`ToOwned`].
 #[allow(non_snake_case)]
 pub fn O<T>(o: &T) -> T::Owned
 where
@@ -27,68 +18,41 @@ where
     ToOwned::to_owned(o)
 }
 
-/// Expect ok or some.
-#[track_caller]
+/// Power [`ToString`].
 #[allow(non_snake_case)]
-pub fn X<T>(v: T) -> T::Output
+pub fn S<T>(s: &T) -> String
 where
-    T: QuickExpect,
+    T: ToString + ?Sized,
 {
-    v.X()
+    ToString::to_string(s)
 }
 
-/// Ignore result.
+/// Power ignore [`Result`] - kick that `Result` to the curb!
 #[allow(non_snake_case)]
 pub fn I<T, E>(r: Result<T, E>) {
     let _ = r;
 }
 
-/// Helper trait for the `X` function.
-pub trait QuickExpect {
-    type Output;
-    #[allow(non_snake_case)]
-    fn X(self) -> Self::Output;
-}
-
-impl<T> QuickExpect for Option<T> {
-    type Output = T;
-
-    #[track_caller]
-    fn X(self) -> T {
-        match self {
-            Some(v) => v,
-            None => panic!("impossible `None` option"),
-        }
-    }
-}
-
-impl<T, E: std::error::Error> QuickExpect for Result<T, E> {
-    type Output = T;
-
-    #[track_caller]
-    fn X(self) -> T {
-        match self {
-            Ok(v) => v,
-            Err(e) => panic!("impossible `Err` result: {e}"),
-        }
-    }
-}
-
-/// To `String`.
-#[extension_trait::extension_trait]
-pub impl<T> QuickToString for T
+/// Power `expect`.
+#[track_caller]
+#[allow(non_snake_case)]
+pub fn X<T>(v: T) -> T::Output
 where
-    T: ToString + ?Sized,
+    T: PowerExpect,
 {
-    #[allow(non_snake_case)]
-    fn S(&self) -> String {
-        ToString::to_string(self)
-    }
+    v.X()
 }
 
-/// Clone.
+//                //
+// -------------- //
+//                //
+
+// ^ those are barbells, for power
+
+
+/// Power [`Clone`].
 #[extension_trait::extension_trait]
-pub impl<T> QuickClone<T> for T
+pub impl<T> PowerClone<T> for T
 where
     T: Clone,
 {
@@ -98,9 +62,9 @@ where
     }
 }
 
-/// To owned.
+/// Power [`ToOwned`].
 #[extension_trait::extension_trait]
-pub impl<T> QuickToOwned for T
+pub impl<T> PowerToOwned for T
 where
     T: ToOwned + ?Sized,
 {
@@ -112,16 +76,55 @@ where
     }
 }
 
+/// Power [`ToString`].
+#[extension_trait::extension_trait]
+pub impl<T> PowerToString for T
+where
+    T: ToString + ?Sized,
+{
+    #[allow(non_snake_case)]
+    fn S(&self) -> String {
+        ToString::to_string(self)
+    }
+}
 
-/// Ignore a `Result`.
-///
-/// This is nice because the common idiom of `let _ = ...` is untyped
-/// and can accidentally be applied to non-`Result` types like `Future`s.
+/// Power ignore [`Result`] - kick that `Result` to the curb!
 #[extension_trait::extension_trait]
 pub impl<T, E> ResultIgnore<T, E> for Result<T, E> {
     #[track_caller]
     #[allow(non_snake_case)]
     fn I(self) {
         let _ = self;
+    }
+}
+
+/// Power `expect`.
+pub trait PowerExpect {
+    type Output;
+    #[allow(non_snake_case)]
+    fn X(self) -> Self::Output;
+}
+
+impl<T> PowerExpect for Option<T> {
+    type Output = T;
+
+    #[track_caller]
+    fn X(self) -> T {
+        match self {
+            Some(v) => v,
+            None => panic!("impossible `None` option"),
+        }
+    }
+}
+
+impl<T, E: std::error::Error> PowerExpect for Result<T, E> {
+    type Output = T;
+
+    #[track_caller]
+    fn X(self) -> T {
+        match self {
+            Ok(v) => v,
+            Err(e) => panic!("impossible `Err` result: {e}"),
+        }
     }
 }
